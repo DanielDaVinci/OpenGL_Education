e2858ab197c93599cf6393e2b28e3075c44235e9
Daniel da Vinci <daniil11072003@mail.ru>
2023-04-10 09:37:34 +0300

    Use uniform color

--- WindowGL.cpp
+++ WindowGL.cpp
@@ -25,10 +25,11 @@ const GLchar* vertexShaderSource = "#version 330 core\n"
 	"}\0";
 
 const GLchar* fragmentShaderSource = "#version 330 core\n"
-	"\out vec4 color;\n"
+	"out vec4 color;\n"
+    "uniform vec4 ourColor;\n"
 	"void main()\n"
 	"{\n"
-	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"color = ourColor;\n"
 	"}\n\0";
 
 int main()
@@ -97,24 +98,19 @@ int main()
 
     GLfloat vertices[] = {
         -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f, 
-        -0.5f,  0.5f, 0.0f   
+         0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
     };
-    GLuint indices[] = {
-        0, 1, 2,
-    };
-    GLuint VBO, EBO, VAO;
 
-    glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    GLuint VBO, VAO;
 
     glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
     glEnableVertexAttribArray(0);
@@ -123,19 +119,29 @@ int main()
 
     glBindVertexArray(0);
 
+
 	while (!glfwWindowShouldClose(window))
 	{
 		glfwPollEvents();
 
 		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
 		glUseProgram(shaderProgram);
+
+        GLfloat timeValue = glfwGetTime();
+        GLfloat greenValue = (sin(timeValue) / 2) + 0.5;
+        GLint vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        glUniform4f(vertexColorLocation, greenValue, greenValue, greenValue, 1.0f);
+
 		glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 	return 0;
